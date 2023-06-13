168ba8682480966f60a73d62d1192a9566378552
EngPeterAtef <peter_atef@stud.cu.edu.eg>
2023-06-13 17:09:20 +0300

    first commit

---
+++ lib/utils/strings.dart
@@ -0,0 +1,14 @@
+const String name = 'Omprakash Chauhan';
+const String role = 'Flutter Developer';
+const String about =
+    "My name is Omprakash, I'm 23 years old, and I live in Mumbai. I am currently studying at a university. I've also been studying a lot on my own to learn new technologies and skills.\n\nI started as a Software Engineering Trainee in 2021. I have been dedicating about 4-5 hrs a day to code. ";
+//  URL
+const String github = 'https://github.com/om-chauhan';
+const String linkedin = 'https://linkedin.com/in/omprakash-chauhan/';
+const String twitter = 'https://twitter.com/_Omi_Op';
+const String emailId = 'omi8655@gmail.com';
+const String miStoreIcon = 'assets/images/getApps_badge.png';
+const String googlePlayIcon = 'assets/images/google-play-badge.png';
+const String websiteIcon = 'assets/images/website.png';
+const String githubIcon = 'assets/images/github.png';
+const String apkDownloadLink = 'https://www.om-chauhan.co.in/demo/app-release.apk';
